f0b74086b0fa51f22af4476260cf7fc848422d47
JJuiceCode <97207238+JJuiceCode@users.noreply.github.com>
2024-02-02 20:27:41 +0900

    CoinTracker App test ver.1

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+// https://api.coinpaprika.com/v1/tickers?limit=100
 function CoinTracker() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>Coin Tracker 앱</h1>
+      <h1>Coin Tracker 앱 {coins.length}</h1>
+      {loading ? <h3>LOADING</h3> : null}
+      <ul>
+        {coins.map((coin, index) => <li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} $</li>)}
+      </ul>
     </div>
   );
 }
