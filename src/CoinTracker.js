3d716a46da2469f6b2296c9b2e45bea912470331
JJuiceCode <97207238+JJuiceCode@users.noreply.github.com>
2024-02-05 15:02:26 +0900

    CoinTracker completed

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 function CoinTracker() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [selectedCoinPrice, setSelectedCoinPrice] = useState(0);
+  const [havePrice, setHavePrice] = useState(0)
+  const [isCompleted, setCompleted] = useState(false);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
       .then((response) => response.json())
@@ -11,13 +14,46 @@ function CoinTracker() {
         setLoading(false);
       });
   }, []);
+
+  const onSelectChange = (event) => setSelectedCoinPrice(event.target.value);
+
+  const onChange = (event) => setHavePrice(event.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setCompleted(true);
+    console.log(havePrice / selectedCoinPrice);
+  };
   return (
     <div>
       <h1>Coin Tracker 앱 {coins.length}</h1>
       {loading ? <h3>LOADING</h3> : null}
-      <ul>
-        {coins.map((coin, index) => <li key={index}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} $</li>)}
-      </ul>
+      <form>
+        <div>
+          현재 당신이 가지고 있는 달러를 적으시오 : 
+          <input
+            value={havePrice}
+            onChange={onChange}
+            type="number"
+            placeholder="$"
+          ></input>
+        </div>
+        <select id="coin-tracker" name="coin-tracker" onChange={onSelectChange}>
+          {coins.map((coin, index) => (
+            <option value={coin.quotes.USD.price} key={index}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} $
+            </option>
+          ))}
+        </select>
+        {/* <div>
+          <button type="submit" onClick={onSubmit}>
+            코인 변환기
+          </button>
+        </div> */}
+      </form>
+        <div>
+          당신이 가진 달라로는 {Math.round(havePrice / selectedCoinPrice)}개의 코인을 구매할 수 있습니다.
+        </div> 
     </div>
   );
 }
